06ad242f7b9412effedc08c8c81940c03f1cfb36
SOVA <sova16tais@gmail.com>
2024-12-01 22:13:06 +0300

    Did my homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О сотрудника - "+fullName);
+
+        System.out.println("Задание 2");
+
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullName.toUpperCase() );
+
+        System.out.println("Задание 3");
+
+        String fullName2 = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф.И.О. сотрудника — " + fullName2.replace( 'ё', 'е'));
     }
 }
